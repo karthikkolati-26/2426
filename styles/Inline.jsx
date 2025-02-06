de50a857183db0008461f20b10ed2be9e8c28e9d
karthikkolati-26 <kolatikarthik26@gmail.com>
2025-02-06 18:48:12 +0530

    Add files via upload

---
+++ styles/Inline.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Inline = () => {
+  return (
+   <>
+   <h1 style={{background:"red" , textAlign:"center" , textDecoration : "underline" }}>hello world</h1>
+   
+   </>
+  )
+}
+
+export default Inline
